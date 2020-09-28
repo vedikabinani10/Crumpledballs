090a29ecb078bf03eeea619d65baa07e08029ee6
vedikabinani10 <69752144+vedikabinani10@users.noreply.github.com>
2020-09-28 22:03:59 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,9 +1,10 @@
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
+const matter = Matter.Bodies;
 const Body = Matter.Body;
 var paperball;
-var dustbin1, dustbin2, dustbin3;
+var dustbin2, dustbin3;
 var ground;
 
 function preload()
@@ -19,7 +20,6 @@ function setup() {
 
 	paperball = new Paper(100,600);
 
-	dustbin1 = new Dustbin(400,650,200,20);
 	dustbin2 = new Dustbin(300,600,20,100);
 	dustbin3 = new Dustbin(500,600,20,100);
 
@@ -31,19 +31,20 @@ function setup() {
 
 function draw() {
   rectMode(CENTER);
-
+  Engine.update(engine);
   background(0);
   paperball.display();
-  dustbin1.display();
   dustbin2.display();
   dustbin3.display();
 
   rect(ground.position.x,ground.position.y,800,20);
+  keyPressed();
+  drawSprites();
 }
 
 function keyPressed(){
 	if(keyCode===UP_ARROW){
-		Matter.Body.applyForce(paperball.body,paperball.body.position,{x:85,y:-85});
+		Matter.Body.applyForce(paperball.body,paperball.body.position,{x:5,y:-5});
 	}
 }
 
